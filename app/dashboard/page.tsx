548301d94a69935c60892625ac1e1e474236866f
krit.pra <krit.pra@ascendcorp.com>
2023-12-07 15:48:10 +0700

    feat : setup theme dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,40 @@
+"use client";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import PageContainer from "@/components/container/PageContainer";
+// components
+import SalesOverview from "@/components/dashboard/SalesOverview";
+import YearlyBreakup from "@/components/dashboard/YearlyBreakup";
+import RecentTransactions from "@/components/dashboard/RecentTransactions";
+import ProductPerformance from "@/components/dashboard/ProductPerformance";
+import MonthlyEarnings from "@/components/dashboard/MonthlyEarnings";
+
+export default function Dashboard() {
+  return (
+    <PageContainer title="Dashboard" description="this is Dashboard">
+      <Box mt={3}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} lg={8}>
+            <SalesOverview />
+          </Grid>
+          <Grid item xs={12} lg={4}>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <YearlyBreakup />
+              </Grid>
+              <Grid item xs={12}>
+                <MonthlyEarnings />
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={12} lg={4}>
+            <RecentTransactions />
+          </Grid>
+          <Grid item xs={12} lg={8}>
+            <ProductPerformance />
+          </Grid>
+        </Grid>
+      </Box>
+    </PageContainer>
+  );
+}
